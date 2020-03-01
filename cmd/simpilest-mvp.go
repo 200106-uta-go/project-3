3a5e5310fa8e3bf2ee16e7cda4cc4f20e05812a6
JesseyBland <jesseybland@gmail.com>
2020-03-01 17:16:35 -0600

    MVP + script

--- cmd/simpilest-mvp.go
+++ cmd/simpilest-mvp.go
@@ -16,11 +16,21 @@ func init() {
 }
 
 func main() {
-	command := exec.Command("helm", args...)
-	command.Stderr = os.Stderr
-	out, err := command.Output()
-	if err != nil {
-		log.Printf("Command Failed :: %s", err)
+	//Which istio install package do you want?
+	//demo setup
+	fmt.Println("=======================KREATE=========================")
+	fmt.Println("Deploy istio, grafana, jaeger into cluster? y/n")
+	fmt.Scan(&yes)
+	if again == "y" || again == "Y" || again == "Yes" || again == "yes" {
+
+		command := exec.Command("./scripts/setup.sh")
+		command.Stderr = os.Stderr
+		out, err := command.Output()
+		if err != nil {
+			log.Printf("Command Failed :: %s", err)
+		}
+		fmt.Print(string(out))
+
 	}
-	fmt.Print(string(out))
+
 }
---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+##setup istio
+sudo curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.4.5
+export PATH=$PWD/bin:$PATH
+sudo curl -L https://get.helm.sh/helm-v2.16.3-linux-amd64.tar.gz -o helm.tar.gz
+tar xf helm.tar.gz
+cd linux-amd64/
+sudo cp helm /bin/helm
+sudo cp tiller /bin/tiller
+cd ..
+sudo kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
+sudo helm init --service-account tiller
+helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system 
+kubectl -n istio-system wait --for=condition=complete job --all
+##demo
+helm install install/kubernetes/helm/istio --name istio --namespace istio-system \
+    --values install/kubernetes/helm/istio/values-istio-demo.yaml
