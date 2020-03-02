9b9ceffa584d527e9d0508f5ada0954ed8ecc8b4
JosephZoeller <joe.h.zoeller@gmail.com>
2020-03-02 15:56:22 -0600

    expanded execHandler logic

--- pkg/executil/executil.go
+++ pkg/executil/executil.go
@@ -3,17 +3,39 @@ package executil
 import (
 	"os/exec"
 	"strings"
+	"time"
 )
 
 // ExecHandler handles script commands using sh
-func ExecHandler(bashIn string, index int) (int, error) {
-	s := strings.Split(bashIn, "\n")
-	lens := len(s)
-	for i := index; i < lens; i++ {
-		_, err := exec.Command("/bin/sh", "-c", "\""+s[i]+"\"").Output()
-		if err != nil {
-			return i, err
+func ExecHandler(bashInput string, errTryCount, errWaitCount int) (int, error) {
+	// breaks input into lines
+	s := strings.Split(bashInput, "\n")
+
+	// iterate through lines
+	for i, ln := range s {
+		// execute current line
+		er := execLine(ln)
+
+		// if line error occurs, retry it errTryCount times
+		if er != nil {
+			for i := 0; i < errTryCount; i++ {
+				// before retry, sleep for errWaitCount seconds
+				time.Sleep(time.Second * errWaitCount)
+				// attempt to execute again
+				er = execLine(ln)
+				// success will break out of the error catching loop
+				if er == nil {
+					break
+				}
+			}
+			// if an error occurred more than errTryCount times, return i, er
+			return i, er
 		}
 	}
-	return lens, nil
+	// all good
+	return -1, nil
+}
+
+func execLine(bashLn string) error {
+	_, err := exec.Command("/bin/sh", "-c", "\""+bashLn+"\"").Output()
 }
