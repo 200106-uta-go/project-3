5efa7d96d60e26c0292714088d92c9e29b70ea67
Emilio Feliciano <FelicianoEJ@users.noreply.github.com>
2020-03-02 15:15:45 -0600

    Reworke executil

--- cmd/simpilest-mvp.go
+++ cmd/simpilest-mvp.go
@@ -7,6 +7,25 @@ import (
 	"os"
 	"os/exec"
 	"time"
+
+	"github.com/200106-uta-go/project-3/pkg/executil"
+)
+
+const (
+	script1 = `
+	sudo curl -L https://istio.io/downloadIstio | sh -
+	cd istio-1.4.5
+	export PATH=$PWD/bin:$PATH
+	sudo curl -L https://get.helm.sh/helm-v2.16.3-linux-amd64.tar.gz -o helm.tar.gz
+	tar xf helm.tar.gz
+	cd linux-amd64/
+	sudo cp helm /bin/helm
+	sudo cp tiller /bin/tiller
+	cd ..
+	sudo kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
+	sudo helm init --service-account tiller
+	`
+	script2 = ``
 )
 
 var args []string
@@ -26,12 +45,13 @@ func main() {
 	if again == "y" || again == "Y" || again == "Yes" || again == "yes" {
 
 		fmt.Println("Deploying services to cluster. This may take upto few minutes.")
-		command1 := exec.Command("sh", "./setup1.sh")
-		command1.Stderr = os.Stderr
-		out1, err1 := command1.Output()
-		errorHandler(err1)
-		fmt.Print(string(out1))
-		for (i := 1; i <= 3; i++) {
+		//command1 := exec.Command("sh", "./setup1.sh")
+		//command1.Stderr = os.Stderr
+		//out1, err1 := command1.Output()
+		_, err := executil.ExecHandler(script1, 0)
+		errorHandler(err)
+		//fmt.Print(string(out1))
+		for i := 1; i <= 3; i++ {
 			fmt.Println("Please wait. Still deploying services.")
 			time.Sleep(time.Duration(10) * time.Second)
 		}
--- pkg/executil/executil.go
+++ pkg/executil/executil.go
@@ -1,13 +1,19 @@
 package executil
 
 import (
-	"os"
 	"os/exec"
 	"strings"
 )
 
 // ExecHandler handles script commands using sh
-func ExecHandler(bashIn string) ([]byte, error) {
-	out, err := exec.Command("/bin/sh", "-c", "\"" + bashIn + "\"").Output()
-	return out, err
+func ExecHandler(bashIn string, index int) (int, error) {
+	s := strings.Split(bashIn, "\n")
+	lens := len(s)
+	for i := index; i < lens; i++ {
+		_, err := exec.Command("/bin/sh", "-c", "\""+s[i]+"\"").Output()
+		if err != nil {
+			return i, err
+		}
+	}
+	return lens, nil
 }
