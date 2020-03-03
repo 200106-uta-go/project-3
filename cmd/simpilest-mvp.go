10d157317c12b3c2a2e962c28d63d2a340e365b2
JosephZoeller <joe.h.zoeller@gmail.com>
2020-03-02 22:31:46 -0600

    reimplementing MVP

--- cmd/simpilest-mvp.go
+++ cmd/simpilest-mvp.go
@@ -2,34 +2,6 @@ package main
 
 import (
 	"fmt"
-<<<<<<< Updated upstream
-
-	"github.com/200106-uta-go/project-3/pkg/executil"
-)
-
-const (
-	script = `sudo curl -L https://istio.io/downloadIstio -o istio.sh
-	ls -l
-	sh istio.sh
-	cd istio-1.4.5
-	export PATH=$PWD/bin:$PATH
-	sudo curl -L https://get.helm.sh/helm-v2.16.3-linux-amd64.tar.gz -o helm.tar.gz
-	tar xf helm.tar.gz
-	cd linux-amd64/
-	sudo cp helm /bin/helm
-	sudo cp tiller /bin/tiller
-	cd ..
-	sudo kubectl apply -f install/kubernetes/helm/helm-service-account.yaml
-	sudo helm init --service-account tiller
-	helm install install/kubernetes/helm/istio-init --name istio-init --namespace istio-system
-	kubectl -n istio-system wait --for=condition=complete job --all
-	helm install install/kubernetes/helm/istio --name istio --namespace istio-system --values install/kubernetes/helm/istio/values-istio-demo.yaml`
-
-	script2 = `echo "Hello World"
-	sleep 2
-	echo "World Hello"`
-)
-=======
 	"io/ioutil"
 	"log"
 	"os"
@@ -37,7 +9,6 @@ const (
 )
 
 const filename = "tempSetup.sh"
->>>>>>> Stashed changes
 
 const script = `echo HELLO` // script goes here
 
@@ -52,52 +23,6 @@ func main() {
 	fmt.Scan(&again)
 	if again == "y" || again == "Y" || again == "Yes" || again == "yes" {
 
-<<<<<<< Updated upstream
-		fmt.Println("Deploying services to cluster. This may take upto few minutes.")
-		//command1 := exec.Command("sh", "./setup1.sh")
-		//command1.Stderr = os.Stderr
-		//out1, err1 := command1.Output()
-
-		executil.ExecHandler(script)
-		// lineNumber, err := executil.ExecHandler(script1, 0)
-		// if err != nil {
-		// 	log.Printf("Restarting Script at Line => %d", lineNumber)
-		// 	newLIne, errNew := executil.ExecHandler(script1, lineNumber)
-		// 	if err != nil {
-		// 		log.Printf("Restarting script AGAIN at Line => %d\n Error => %v\n", newLIne, errNew)
-		// 		newLine2, errNew2 := executil.ExecHandler(script1, newLIne)
-		// 		if err != nil {
-		// 			log.Printf("No more attemps. Line Number => %d\nError => %v", newLine2, errNew2)
-		// 		}
-		// 	}
-		// }
-		//fmt.Print(string(out1))
-
-		// for i := 1; i <= 3; i++ {
-		// 	fmt.Println("Please wait. Still deploying services.")
-		// 	time.Sleep(time.Duration(10) * time.Second)
-		// }
-		// command2 := exec.Command("sh", "./setup2.sh")
-		// command2.Stderr = os.Stderr
-		// out2, err2 := command2.Output()
-		// errorHandler(err2)
-		// fmt.Print(string(out2))
-
-		// lineNumberPart2, errPart2 := executil.ExecHandler(script2, 0)
-		// if errPart2 != nil {
-		// 	log.Printf("Restarting Script at Line => %d", lineNumberPart2)
-		// 	newLIne, errNew := executil.ExecHandler(script2, lineNumberPart2)
-		// 	if err != nil {
-		// 		log.Printf("Restarting script AGAIN at Line => %d\n Error => %v\n", newLIne, errNew)
-		// 		newLine2, errNew2 := executil.ExecHandler(script2, newLIne)
-		// 		if err != nil {
-		// 			log.Printf("No more attemps. Line Number => %d\nError => %v", newLine2, errNew2)
-		// 		}
-		// 	}
-		// }
-
-	}
-=======
 		er := ioutil.WriteFile(filename, []byte(script), 0777)
 		if er != nil {
 			log.Fatal("Script failed to deploy - ", er)
@@ -115,5 +40,4 @@ func main() {
 		os.Remove(filename)
 	}
 
->>>>>>> Stashed changes
 }
