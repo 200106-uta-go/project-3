b950dbd4ec5f0e3b082232073895ef833030b8b7
mattackard <mattackard@gmail.com>
2020-03-04 11:11:22 -0600

    add profile struct

--- pkg/kreate/profile.go
+++ pkg/kreate/profile.go
@@ -1,3 +1,18 @@
 package kreate
 
-// defines the profile struct and converts between the struct format and the yaml format
+// Profile defines the profile struct and converts between the struct format and the yaml format
+type Profile struct {
+	Name         string
+	ClusterName  string
+	ClusterIP    string
+	ClusterPorts []string
+	Apps         []App
+}
+
+// App ...
+type App struct {
+	Name      string
+	ImageURL  string
+	Ports     []string
+	Endpoints []string
+}
