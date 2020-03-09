7777facf4e449400f98746de4285c8255a2bf3e2
mattackard <mattackard@gmail.com>
2020-03-09 17:20:03 -0500

    remove outline text at top of file

--- pkg/kreate/createchart.go
+++ pkg/kreate/createchart.go
@@ -10,15 +10,6 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-/*
-## kreate chart <profile name>
-1. Part 1. (to be reused within kreate run)
-    - The specified profile must be converted from its profile format to the helm chart's values.yaml format
-    - The created values.yaml must then be copied to the kustom chart.
-2. Part 2.
-    - The kustom chart must then be copied to a user-friendly directory (implementation as descretioned by developer)
-*/
-
 //CreateChart creates a helm chart using the data provided in profile
 func CreateChart(profileName string) {
 
