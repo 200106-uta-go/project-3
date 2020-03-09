3435499b719f0d3687cff0afa9ebd79137df67f2
Joshua Nguyen <jnguyen5039@gmail.com>
2020-03-08 21:30:31 -0600

    updated flag description.

--- cmd/kreate/usage.go
+++ cmd/kreate/usage.go
@@ -14,6 +14,17 @@ The commands are:
 		init		sets up necessary dependencies
 		profile		create a new profile which the user can set key value pairs in "etc/kreate"
 		edit		edit a pre-existing profile
+			-name	Sets the name of profile
+			-clustername	Sets the clustername of the profile
+			-clusterip	Sets the clusterip of the profile
+			-clusterport	Append a clusterport to the profile
+			
+			-NameOfApp	Specifies the name of the app which will be modified by the app-related input flags
+			-imageurl	An App-related flag. Sets the imageurl of the App specified by the NameOfApp flag
+			-servicename	An App-related flag. Sets the servicename of the App specified by the NameOfApp flag
+			-serviceport	An App-related flag. Sets the serviceport of the App specified by the NameOfApp flag
+			-port	An App-related flag. Appends a port to the App specified by the NameOfApp flag
+			-endpoint	An App-related flag. Appends an endpoint to the App specified by the NameOfApp flag	
 		chart		will use a profile to produce a preconfigured chart that the user may use with his verstion of helm
 		remove		remove selected profile from system
 			-a, --all	removes all profiles from system
